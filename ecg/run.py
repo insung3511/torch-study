80dbb818ef74f36b38a9cb8f7e03bc2cb922d729
insung3511 <insung.park123@gmail.com>
2022-08-31 17:05:38 +0900

    Still matic...

---
+++ ecg/run.py
@@ -0,0 +1,12 @@
+import torch.nn.functional as F
+import torch.nn as nn
+import torch
+
+from dataloader import Dataset
+from model import Model
+
+model = Model()
+datasets = Dataset()
+
+train_data = datasets.train_data()
+test_data = datasets.test_data()
