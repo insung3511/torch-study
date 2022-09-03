3ebaa24eb031aa1802775584a9c38c395d85f34f
Restyled.io <commits@restyled.io>
2022-09-03 08:34:53 +0000

    Restyled by yapf

--- ecg/dataloader.py
+++ ecg/dataloader.py
@@ -7,19 +7,22 @@ from torchvision.transforms import ToTensor
 
 
 class Dataset:
+
     def __init__(self):
         self.root = root
 
     def train_data(root):
-        training_data = datasets.FashionMNIST(
-            root=root, train=True, download=True, transform=ToTensor()
-        )
+        training_data = datasets.FashionMNIST(root=root,
+                                              train=True,
+                                              download=True,
+                                              transform=ToTensor())
 
         return training_data
 
     def test_data(root):
-        test_data = datasets.FashionMNIST(
-            root=root, train=False, download=True, transform=ToTensor()
-        )
+        test_data = datasets.FashionMNIST(root=root,
+                                          train=False,
+                                          download=True,
+                                          transform=ToTensor())
 
         return test_data
